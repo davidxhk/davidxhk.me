2500cb9518d2a5799869da51189f6177f5c9c3fe
David Xie <hk997@live.com>
2025-06-04 17:16:52 +0800

    lint cn utility

--- src/utils/cn.ts
+++ src/utils/cn.ts
@@ -1,13 +1,14 @@
-import type { ClassValue } from "clsx";
+import type { ClassValue } from "clsx"
 
-import { clsx } from "clsx";
-import { twMerge } from "tailwind-merge";
+import { clsx } from "clsx"
+import { twMerge } from "tailwind-merge"
 
 /**
  * Combines multiple class names or conditional class names and merges Tailwind CSS classes efficiently
+ *
  * @param classes - Array of class names, objects, or falsy values
  * @returns Merged and deduplicated className string
  */
 export function cn(...classes: ClassValue[]): string {
-  return twMerge(clsx(...classes));
+  return twMerge(clsx(...classes))
 }
