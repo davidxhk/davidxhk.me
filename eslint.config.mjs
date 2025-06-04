c106290ead05ac986ced2728fae8c2db31be574d
David Xie <hk997@live.com>
2025-06-04 14:32:20 +0800

    install eslint prettier plugin

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,16 +1,42 @@
-import { dirname } from "path";
-import { fileURLToPath } from "url";
-import { FlatCompat } from "@eslint/eslintrc";
+import { FlatCompat } from "@eslint/eslintrc"
+import { dirname } from "path"
+import { fileURLToPath } from "url"
 
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = dirname(__filename);
+const __filename = fileURLToPath(import.meta.url)
+const __dirname = dirname(__filename)
 
 const compat = new FlatCompat({
   baseDirectory: __dirname,
-});
+})
 
 const eslintConfig = [
-  ...compat.extends("next/core-web-vitals", "next/typescript"),
-];
+  ...compat.config({
+    extends: [
+      "next",
+      "next/core-web-vitals",
+      "next/typescript",
+      "plugin:prettier/recommended",
+    ],
+    plugins: ["prettier"],
+    rules: {
+      "prettier/prettier": [
+        "warn",
+        {
+          semi: false,
+          singleQuote: false,
+          trailingComma: "all",
+          arrowParens: "always",
+          endOfLine: "lf",
+          plugins: ["prettier-plugin-tailwindcss"],
+          tailwindStylesheet: "./src/app/globals.css",
+          tailwindFunctions: ["cn"],
+        },
+        {
+          usePrettierrc: false,
+        },
+      ],
+    },
+  }),
+]
 
-export default eslintConfig;
+export default eslintConfig
