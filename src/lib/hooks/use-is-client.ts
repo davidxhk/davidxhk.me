5d0c0f0ac656babcbf2e7a387b08de0e3e550444
David Xie <hk997@live.com>
2025-06-09 13:48:08 +0800

    add is client hook

---
+++ src/lib/hooks/use-is-client.ts
@@ -0,0 +1,13 @@
+"use client"
+
+import { useEffect, useState } from "react"
+
+export function useIsClient(): boolean {
+  const [isClient, setClient] = useState(false)
+
+  useEffect(() => {
+    setClient(true)
+  }, [])
+
+  return isClient
+}
